e7e2d91a41e54ba9d090c58f12c5834563608939
pby1360 <pby1360@gmail.com>
2021-06-23 23:57:59 +0900

    modify style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,36 @@
 import React from "react";
 import { Link } from  "react-router-dom";
 
-const wrapper = {
-  dsiplay: "flex",
-};
-
 const Navigation = () => {
   return (
     <section class="navigation-wrap">
-      <article>
-        <Link to="/">메인</Link>
-      </article>
-      <article>
-        <Link to="/help">소개</Link>
-      </article>
-      <article>
-        <Link to="/product">유료서비스</Link>
-      </article>
-      <article>
-        <Link to="/login">로그인</Link>
-      </article>
+      <section class="navigation-logo">
+        <article>
+            <Link to="/">업로드</Link>
+        </article>
+      </section>
+      <section class="navigation-menu">        
+        <article class="navigation-item">
+          <Link to="/help">소개</Link>
+        </article>
+        <article class="navigation-item">
+          <Link to="/help">컨텐츠</Link>
+        </article>
+        <article class="navigation-item">
+          <Link to="/help">배우기</Link>
+        </article>
+        <article class="navigation-item">
+          <Link to="/product">유료서비스</Link>
+        </article>
+      </section>
+      <section class="navigation-login">
+        <article class="navigation-item login">
+          <Link to="/login">로그인</Link>
+        </article>
+        <article class="navigation-item login">
+          <Link to="/login">회원가입</Link>
+        </article>
+      </section>
     </section>
   )
 }
