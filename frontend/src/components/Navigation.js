c276c04c0eca40499d8986f6bdc8efa5124b7363
BY <pby1360@gmail.com>
2021-07-13 00:44:56 +0900

    modify home

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => {
     <section className="navigation-wrap">
       <section className="navigation-logo">
         <article>
-          <Link to="/"><img className="navigation-logo-img" src="/assets/uproad_logo2.png" alt="logo" /></Link>
+          <Link to="/"><img className="navigation-logo-img" src="/assets/uproad_logo3.png" alt="logo" /></Link>
         </article>
       </section>
       <section className="navigation-menu">        
@@ -17,22 +17,22 @@ const Navigation = () => {
         <article className="navigation-item">
           <Link to="/help">컨텐츠</Link>
         </article>
-        <article className="navigation-item">
+        {/* <article className="navigation-item">
           <Link to="/help">컨텐츠</Link>
-        </article>
-        <article className="navigation-item">
+        </article> */}
+        {/* <article className="navigation-item">
           <Link to="/help">커뮤니티</Link>
-        </article>
+        </article> */}
         <article className="navigation-item">
           <Link to="/product">프리미엄</Link>
         </article>
       </section>
       <section className="navigation-login">
         <article className="navigation-item login">
-          <Link to="/login"><Button variant="contained">로그인</Button></Link>
+          <Link to="/login"><Button variant="outlined" color="primary">로그인</Button></Link>
         </article>
         <article className="navigation-item login">
-          <Link to="/login"><Button variant="contained">회원가입</Button></Link>
+          <Link to="/login"><Button variant="contained" color="primary">회원가입</Button></Link>
         </article>
       </section>
     </section>
