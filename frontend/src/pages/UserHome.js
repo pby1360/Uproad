2d89b7cc8a01fbe9d14ceefc6774c7772b0e344b
BY <pby1360@gmail.com>
2021-08-09 23:20:26 +0900

    add scoped style modules

--- frontend/src/pages/UserHome.js
+++ frontend/src/pages/UserHome.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "../styles/UserHome.scoped.scss";
 
 const UserHome = () => {
   return (
     <div>
       <section className="user-home-container">
         <section className="top-banner">Banner</section>
+        <section className="center-profile">User dashboard</section>
         <section className="bottom-dashboard">User dashboard</section>
       </section>      
     </div>
