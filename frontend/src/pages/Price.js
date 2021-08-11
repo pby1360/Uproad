0325a247935325917ec64700cd35efa82cd4f24c
soojin Kim <so_ojin0914@naver.com>
2021-08-11 22:42:01 +0900

    구독료 화면 구성 #2

--- frontend/src/pages/Price.js
+++ frontend/src/pages/Price.js
@@ -1,44 +1,54 @@
 import React from "react";
+import PriceStyle from "../styles/Price.scss";
+
 
 const Price = () => {
   return (
-    <section>
-      <article className="price-content-info">
-        <section className="price-content-info-phrase">
+    <div className={PriceStyle}>
+      <section className="price-content-info">
+        <article className="price-content-info-phrase">
           <h1>구독료</h1>
           <h2>원하는 카테고리를 선택하세요.</h2>
           <h2>카테고리 갯수에 따라 할인이 적용됩니다.</h2>
-          <div className="price-select-content">
-            <section style={{display: "flex"}}>
-              <article style={{flex: "1"}}>
-                <ul style={{listStyle: "none"}}>
-                  <li>1</li>
-                  <li>2</li>
-                  <li>3</li>
-                  <li>4</li>
-                </ul>
-              </article>
-              <article style={{flex: "1"}}>
-                <ul style={{listStyle: "none"}}>
-                  <li>1</li>
-                  <li>2</li>
-                  <li>3</li>
-                  <li>4</li>
-                </ul>
-              </article>
-              <article style={{flex: "1"}}>
-                <ul style={{listStyle: "none"}}>
-                  <li>1</li>
-                  <li>2</li>
-                  <li>3</li>
-                  <li>4</li>
-                </ul>
-              </article>
+
+          <article className="price-select-content">
+            <section className="monthly-price-title">
+              <section className="title">월 구독료</section>
+              <span className="total-price">7000</span>
+              <section className="title">원</section>
             </section>
-          </div>
-        </section>
-      </article>
-    </section>
+            <article style={{display: "flex"}}>
+              <article style={{flex: "1"}}>
+                <ul style={{listStyle: "none"}}>
+                  <li>1</li>
+                  <li>2</li>
+                  <li>3</li>
+                  <li>4</li>
+                </ul>
+              </article>
+              <article style={{flex: "1"}}>
+                <ul style={{listStyle: "none"}}>
+                  <li>1</li>
+                  <li>2</li>
+                  <li>3</li>
+                  <li>4</li>
+                </ul>
+              </article>
+              <article style={{flex: "1"}}>
+                <ul style={{listStyle: "none"}}>
+                  <li>1</li>
+                  <li>2</li>
+                  <li>3</li>
+                  <li>4</li>
+                </ul>
+              </article>
+            </article>
+          </article>
+        </article>
+      </section>
+
+
+    </div>
   )
 }
 
