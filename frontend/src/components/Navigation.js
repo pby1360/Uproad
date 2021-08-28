f9e49a1ef2239083d5bc2e36ee77f7a2972c80b0
BY <pby1360@gmail.com>
2021-08-28 12:16:34 +0900

    add dropdown menu

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,84 +1,52 @@
-import { makeStyles, Menu, MenuItem } from '@material-ui/core';
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import NavigationStyle from '../styles/components/Navigation.scss';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  // menuItems: {
-  //   textDecorationLine: 'none',
-  //   textDecoration: 'none',
-  //   textUnderlineOffset: true,
-  // }
-}));
-
 const Navigation = () => {
-  const classes = useStyles();
-
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const openMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const closeMenu = (event) => {
-    setAnchorEl(null);
-  };
   
-  const items = () => {
-    return (
-      <Menu
-        className={classes.root}
-        id="simple-menu"
-        anchorEl={anchorEl}
-        getContentAnchorEl={null}
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        transformOrigin={{ vertical: "top", horizontal: "center" }}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={closeMenu}
-        // MenuListProps={{ onMouseLeave: closeMenu }}
-      >
-        <MenuItem>
-          <Link to="/vision">비전</Link>
-        </MenuItem>
-        <MenuItem>
-          <Link to="/help">가치관</Link>
-        </MenuItem>
-        <MenuItem>
-          <Link to="/help">히스토리</Link>
-        </MenuItem>
-        <MenuItem>
-          <Link to="/help">소식</Link>
-        </MenuItem>
-      </Menu>
-    )
-  };
-
   return (
     <div className={NavigationStyle}>
-      {items()}
       <section className="navigation-bar">
         <section className="navigation-logo">
-          <Link to="/"><img className="navigation-logo-img" src="/assets/upRoad_log_pink.png" alt="logo" /></Link>
+          <Link to="/">Uproad</Link>
         </section>
-        <section className="navigation-item">
-          <a onClick={openMenu} className="navigation-item-a" href={false} value="1">업로드 소개</a>
-        </section>
-        <section className="navigation-item">
-          <a onClick={openMenu} className="navigation-item-a" href={false} value="2">챌린지</a>
-        </section>
-        <section className="navigation-item">
-          <a onClick={openMenu} className="navigation-item-a" href={false} value="3">챌린지 가이드</a>
-        </section>
-        <section className="navigation-item">
-          <a className="navigation-item-a" href>스토어</a>
-        </section>
-        <section className="navigation-item">
-          <a className="navigation-item-a" href>마이페이지</a>
+        <nav className="navigation-nav">
+          <ul class="clearfix">
+            <li>
+            <Link to="#">업로드 소개</Link>
+              <ul>
+                <li><Link to="/vision">비전</Link></li>
+                <li><Link to="/help">가치관</Link></li>
+                <li><Link to="/help">히스토리</Link></li>
+                <li><Link to="/help">소식</Link></li>
+              </ul>
+            </li>
+            <li>
+            <Link to="#">챌린지</Link>
+              <ul>
+                <li><Link to="/vision">무작정 챌린지</Link></li>
+                <li><Link to="/vision">만들기 챌린지</Link></li>
+                <li><Link to="/vision">패키지</Link></li>
+              </ul>
+            </li>
+            <li>
+              <Link to="#">챌린지 가이드</Link>
+              <ul>
+                <li><Link to="/vision">참여방법</Link></li>
+                <li><Link to="/vision">참여보상</Link></li>
+                <li><Link to="/vision">자주묻는 질문</Link></li>
+              </ul>
+            </li>
+            <li><Link to="/vision">스토어</Link></li>
+            <li><Link to="/vision">마이페이지</Link></li>
+          </ul>
+        </nav>
+        <section className="navigation-login">
+          <Link to="/">로그인</Link>
+          <Link to="/">회원가입</Link>
         </section>
       </section>
+      <section className="dropdown"></section>
     </div>
   );
 };
