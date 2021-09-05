8ebd66b2c50e79c5ef6a0381e6779ba20f196cd6
soojin Kim <so_ojin0914@naver.com>
2021-09-05 23:05:09 +0900

    업로드 소개 - 가치관 구현 (가치관 문구 추가) #13

--- frontend/src/pages/introduction/About.js
+++ frontend/src/pages/introduction/About.js
@@ -4,11 +4,65 @@ import AboutStyle from "../../styles/introduction/About.scss";
 const About = () => {
   return (
     <div className={AboutStyle}>
-        <div className="about-container">
-        <div className="about-title">
-            <div className="about-title-text">가치관</div>
-        </div>
-    </div>
+      <section className="about-container">
+          <section className="about-title">
+              <div className="about-title-text">가치관</div>
+          </section>
+          <section className="about-text-container">
+            <div className="about-text-emoji-container">
+              <section className="emoji">🚀</section>
+              <section className="about-text-group-left">
+                <section className="about-text-1">우리는 업로드의 가능성을 <br></br>믿습니다.</section>
+                <section className="about-text-2">uproad : 경쟁에서 승리보다는 패배가 예상되는 약자</section>
+                <section className="about-text-3">스타트업은 남들이 안 된다는 걸 할 수 있다고 믿으며 실행하고, 존재하지 않는 걸 만들어냅니다. 그렇기에 기존 시스템에서 바라보았을 때 스타트업은 약자일 수밖에 없으며, 더군다나 사회적 가치를 과정과 결과에 담는 일은 더욱 어렵습니다. 우리는 약자인 혁신 창업가들이 성장하고 성공하기를 진심을 다해 응원하기 위해 ‘underdogs’라는 이름 아래 존재합니다. 언더독들이 모여 판을 뒤집고 더 나은 세상을 만들 수 있다고 믿습니다.</section>
+              </section>
+            </div>
+            <div className="about-text-group-left">
+              <section className="about-text-1">창업가와 함께 시작하고<br></br>성장해나가는 파트너</section>
+              <section className="about-text-3">스타트업은 남들이 안 된다는 걸 할 수 있다고 믿으며 실행하고, 존재하지 않는 걸 만들어냅니다. 그렇기에 기존 시스템에서 바라보았을 때 스타트업은 약자일 수밖에 없으며, 더군다나 사회적 가치를 과정과 결과에 담는 일은 더욱 어렵습니다. 우리는 약자인 혁신 창업가들이 성장하고 성공하기를 진심을 다해 응원하기 위해 ‘underdogs’라는 이름 아래 존재합니다. 언더독들이 모여 판을 뒤집고 더 나은 세상을 만들 수 있다고 믿습니다.</section>
+            </div>
+            <div className="about-text-group-right">
+              <section className="about-text-4">CORE VALUE</section>
+              <section className="about-text-5">언더독스팀은 창업가를 대할 때, 파트너를 대할 때,<br></br>다른 구성원들과 스스로를 대할 때 4가지 핵심 가치를 지켜나갑니다.</section>
+            </div>
+            <section className="about-text-emoji-container">
+              <div className="about-text-group-left">
+                <section className="about-text-6">Creating New Wave</section>
+                <section className="about-text-2">이전에 없던 새로운 길을 찾아 나섭니다.</section>
+                <section className="about-text-3">아무도 가보지 않은 길은 실패의 위험이 도사리고 있지만, 다른 한편으로는 무한한 가능성으로 가득 차 있기도 합니다. 언더독스는 후자의 관점에서 창업가들과 함께 문제를 다시 보고, 새로운 해답을 찾아 나섭니다.</section>
+              </div>
+              <section className="emoji">🚀</section>
+            </section>
+            <section className="about-text-emoji-container">
+              <section className="emoji">🚀</section>
+              <div className="about-text-group-left">
+                <section className="about-text-6">Aiming at Perfection</section>
+                <section className="about-text-2">매 순간 최고의 성과를 내기 위해 노력합니다.</section>
+                <section className="about-text-3">전·현직 창업가들의 성공과 실패, 그리고 연쇄 창업 경험을 기반으로 언더독스만의 차별화된 창업 콘텐츠를 개발합니다. 이를 창업가들에게 최적화된 방식으로 전달하여 최고의 성과를 낼 수 있도록 이끕니다.</section>
+              </div>
+            </section>
+            <section className="about-text-emoji-container">
+              <div className="about-text-group-left">
+                <section className="about-text-6">Freedom & Responsibility</section>
+                <section className="about-text-2">자유를 누리면서 책임감 있는 결과를 만듭니다.</section>
+                <section className="about-text-3">형식에 얽매이지 않으며, 다양하게 또 창의적으로 사고합니다. 그러나 자유에는 항상 큰 책임이 따른다는 점을 마음에 새기고, 원칙과 약속을 반드시 지켜 책임감 있는 결과를 만듭니다.</section>
+              </div>
+              <section className="emoji">🚀</section>
+            </section>
+            <section className="about-text-emoji-container">
+              <section className="emoji">🚀</section>
+              <div className="about-text-group-left">
+                <section className="about-text-6">Acting as One Team</section>
+                <section className="about-text-2">하나의 팀이 되어 더 나은 해답을 찾아갑니다.</section>
+                <section className="about-text-3">어디서부터, 어떻게 시작해야 할지 모르는 창업가들과 함께 고민하고 해답을 찾아갑니다.이러한 창업가들이 하나둘 모여 서로에게 힘이 되어주고 좋은 자극이 되는 국내외 최대 커뮤니티를 이룹니다.</section>
+              </div>
+            </section>
+            <div className="about-text-group-left">
+              <section className="about-text-4">CODE OF START-UP</section>
+              <section className="about-text-2">언더독스는 실제 창업 경험을 통해 얻은 8가지 창업 원칙에 따라 창업교육을 진행하고,<br></br> 창업가들과 함께 비즈니스 성과와 사회적 가치를 달성합니다.</section>
+            </div>
+          </section>
+      </section>
     </div>
   );
 };
