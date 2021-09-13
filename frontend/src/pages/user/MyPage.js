46047c6a6e5d247581cedcf3fa93d78936d46566
DT042104 <DT042104@mobis.co.kr>
2021-09-13 15:11:09 +0900

    마이페이지 구현 #6

--- frontend/src/pages/user/MyPage.js
+++ frontend/src/pages/user/MyPage.js
@@ -48,6 +48,10 @@ function a11yProps(index) {
 const MyPage = ({match}) => {
   const { id } = match.params;
 
+  const preceding = 27;
+  const follower = 2169;
+  const following = 685;
+
   useEffect(() => {
     // async function getUser() {
     //   await axios.get(`/api/user/${id}`)
@@ -70,38 +74,41 @@ const MyPage = ({match}) => {
   return (
     <div className={UserHomeStyles}>
       <section className="user-home-container">
-        <article className="top-banner">Banner</article>
+        <article className="top-banner">배너 이미지 추가</article>
         <article className="center-profile">
           <section className="user-profile-img"><img src="/assets/ceo_info2.jpg" alt="ceo"></img></section>
           <section className="user-profile-info">
-            <section className="user-name">bala un</section>
-            <section className="user-job">디자이너</section>
-            <section className="user-group">WTM</section>
-            <section className="user-region"><Room/>Icheon, korea, Republic of</section>
+            <section className="user-name">언발라</section>
+            <section className="user-id">@unbalance_life</section>
+            <section className="user-subscrip-period">Cafe,For 3 years</section>
           </section>
           <section className="user-profile-buttons">
-            <Button className="edit-profile" href={"/profile/" + id} variant="contained" startIcon={<CreateIcon />}>내 프로필 편집</Button>
-            <Button className="edit-availability" variant="contained" startIcon={<MailOutlineIcon />}>가용성 편집</Button>
-            <Button className="edit-dummy" variant="contained" startIcon={<PictureAsPdfIcon />}>Adobe Portfolio 시험 사용</Button>
-            <section className="following">
-              <span>팔로잉</span>
-              <span className="following-count">9</span>
+            <section className="count-container">
+              <section className="preceding-container">
+                <span>선행</span>
+                <span className="preceding-count">{preceding.toLocaleString()}</span>
+              </section>
+              <section className="follower-container">
+                <span>팔로워</span>
+                <span className="follower-count">{follower.toLocaleString()}</span>
+              </section>
+              <section className="following-container">
+                <span>팔로잉</span>
+                <span className="following-count">{following.toLocaleString()}</span>
+              </section>
             </section>
-          </section>
-          <section className="user-profile-date">
-            <p>멤버 가입일: 2021년 7월 20일</p>
+            <Button className="edit-profile" href={"/profile/" + id} variant="contained">내 프로필 편집</Button>
+            <Button className="edit-availability" variant="contained">커뮤니티 이동하기</Button>
           </section>
         </article>
+        <article className="top-dashboard"></article>
         <article className="bottom-dashboard">
         <article className="user-tabs-dummy"></article>
           <article className="user-tabs">
             <Tabs value={value} onChange={handleChange}>
-              <Tab label="작업" {...a11yProps(0)} />
-              <Tab label="라이브스트림" {...a11yProps(1)} />
-              <Tab label="무드보드" {...a11yProps(2)} />
-              <Tab label="평가" {...a11yProps(3)} />
-              <Tab label="인사이트" {...a11yProps(4)} />
-              <Tab label="초안" {...a11yProps(5)} />
+              <Tab label="참여 챌린지" {...a11yProps(0)} />
+              <Tab label="나의 인증" {...a11yProps(1)} />
+              <Tab label="작성한 글" {...a11yProps(2)} />
             </Tabs>
             <TabPanel value={value} index={0}>
               <section className="card-container">
@@ -129,10 +136,10 @@ const MyPage = ({match}) => {
               </section>
             </TabPanel>
             <TabPanel value={value} index={1}>
-              Item Two
+              <section classNmae="card-container">Item Two</section>
             </TabPanel>
             <TabPanel value={value} index={2}>
-              Item Three
+            <section classNmae="card-container">Item Three</section>
             </TabPanel>
           </article>
         </article>
